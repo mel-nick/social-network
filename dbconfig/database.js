02b0e66805eb1b5e1499913a69b2a114db8cc0c0
Nick Meleshchenko <nick.meleshchenko@gmail.com>
2019-09-18 21:12:22 +0300

    local data base connected

---
+++ dbconfig/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/socialapp'
+    // database: 'mongodb+srv://melnick:melnick123@e-shop-elmlx.mongodb.net/shop?retryWrites=true&w=majority'
+}
--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const config = require('./dbconfig/database');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-
+//app get
 app.get('/', (req, res)=>res.send('API running'))
 
 // Mongoose connection
 const db = mongoose.connection;
+
+//mongoose connect
+mongoose.connect(process.env.MONGODB_URI || config.database, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false} );
 
 // Check connection
 db.once('open', function () {
@@ -19,7 +23,10 @@ db.once('open', function () {
 db.on('error', function (err) {
     console.error(err);
 });
-//listen
+
+
+
+//app listen
 app.listen(PORT, (req, res) => {
   console.log(`Server is listening on port: ${PORT}`)
 })
